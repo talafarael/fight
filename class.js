41ffaef47da24aa2d29f6552ea87b41ee178485a
talafarael <127351345+talafarael@users.noreply.github.com>
2024-06-05 21:14:14 +0300

    left right

--- class.js
+++ class.js
@@ -18,7 +18,7 @@ export class Move extends Person {
 
 	moveForward() {
 		if (this.positionBody.positionBody.x < 725) {
-            console.log('forward')
+			console.log("forward")
 			this.positionBody.positionBody.x =
 				this.positionBody.positionBody.x + 10
 			return this.positionBody.positionBody
@@ -33,53 +33,47 @@ export class Move extends Person {
 		}
 		return this.positionBody.positionBody
 	}
-    
+
 	async jumpUp(rotate) {
 		return new Promise((resolve) => {
-            let positionY=-10
-            let  positionX=0
+			let positionY = -10
+			let positionX = 0
 			let startPosition = this.positionBody.positionBody.y
-			
+
 			const startJump = setInterval(() => {
-                if(rotate==='left'){
-                    positionX=-5
-                }
-                if(rotate==='right'){
-                    positionX=5
-                }
-				this.positionBody.positionBody.y +=positionY
-                this.positionBody.positionBody.x +=positionX
+				if (rotate === "left") {
+					positionX = -5
+				}
+				if (rotate === "right") {
+					positionX = 5
+				}
+				this.positionBody.positionBody.y += positionY
+				this.positionBody.positionBody.x += positionX
 				if (this.stateDoubleJump === 2) {
 					startPosition = this.positionBody.positionBody.y
-                    console.log('di')
-                    positionY=-10
-                    this.stateDoubleJump=0
-                    
-                    
-                    return; 
+					console.log("di")
+					positionY = -10
+					this.stateDoubleJump = 0
+
+					return
 				}
 
 				if (startPosition - 100 > this.positionBody.positionBody.y) {
-                    positionY=+10
-				
-                    
-                      
-							
-					
-				}  
-                
-                if (this.positionBody.positionBody.y === 300) {
-                    console.log('null')
-                            clearInterval(startJump)
-                            this.stateDoubleJump=0
-                    
-                            resolve(true)
-                        }
+					positionY = +10
+				}
+
+				if (this.positionBody.positionBody.y === 300) {
+					console.log("null")
+					clearInterval(startJump)
+					this.stateDoubleJump = 0
+
+					resolve(true)
+				}
 			}, 50)
-            if(this.stateDoubleJump === 1){
-                clearInterval(startJump)
-            }
-            this.stateDoubleJump++
+			if (this.stateDoubleJump === 1) {
+				clearInterval(startJump)
+			}
+			this.stateDoubleJump++
 		})
 	}
 
--- script.js
+++ script.js
@@ -1,56 +1,66 @@
-import { Move, Person } from "./class.js"
+import { Move, Person } from "./class.js";
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-let twoJump=0
+let twoJump = 0;
+let pressedKeys = {}; // Define pressedKeys here
+
 ctx.fillStyle = "green";
 canvas.width = 800;
 canvas.height = 600;
-let jump=''
-let arr={y:300,x:25}
-ctx.fillRect(arr.x,arr.y, 75, 75);
+let jump = '';
+let arr = { y: 300, x: 25 };
+ctx.fillRect(arr.x, arr.y, 75, 75);
 
-const person=new Person(arr)
-const move =new  Move(person)
+const person = new Person(arr);
+const move = new Move(person);
+
 function draw() {
-    // Очистка canvas
+    // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
     ctx.fillRect(arr.x, arr.y, 75, 75);
 }
 
+document.addEventListener('keydown', async (event) => {
+    pressedKeys[event.key.toLowerCase()] = true;
 
-
-
-document.addEventListener('keydown',async (event) => {
- 
     if (event.key === 'd' || event.key === 'D') {
-      jump='right'
-       arr.x= move.moveForward().x
-      console.log('da')
-       draw();
-       jump=''
+        jump = 'right';
+        arr.x = move.moveForward().x;
+        console.log('da');
+        draw();
+        jump = '';
     }
     if (event.key === 'a' || event.key === 'A') {
-      jump='left'
-        arr.x= move.moveBack().x
-        jump=''
+        jump = 'left';
+        arr.x = move.moveBack().x;
+        jump = '';
         draw();
-        
-     }
-     if (event.key === 'w' || event.key === 'W') {
-						if(twoJump<2){
-       let result= move.jumpUp(jump).then((result) => {
-								if (result) {
-									twoJump=0
-								}
-				});
-				twoJump++
-        setInterval(()=>{
-								
-         arr= move.positionBody.positionBody
-         
-          draw();
-        },50)
-       }
-     }
-});  
+    }
+
+    if (event.key.toLowerCase() === 'w') {
+        if (twoJump < 2) {
+            let direction = null;
+            if (pressedKeys['d']) {
+                direction = 'right';
+            } else if (pressedKeys['a']) {
+                direction = 'left';
+            }
+
+            move.jumpUp(direction).then((result) => {
+                if (result) {
+                    twoJump = 0;
+                }
+            });
+            twoJump++;
+            setInterval(() => {
+                arr = move.positionBody.positionBody;
+                draw();
+            }, 50);
+        }
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    delete pressedKeys[event.key.toLowerCase()];
+});
